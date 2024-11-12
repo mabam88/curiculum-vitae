2352369936271108c9c3e8639fb746e3e312df65
mabam88 <mabam8803@gmail.com>
2024-11-12 23:35:47 +0800

    upload cv resume

---
+++ cv.js
@@ -0,0 +1,19 @@
+        function redirInstagram() {
+            window.location.href = "https://instagram.com/abafodz"
+        }
+        
+        function redirWhatsapp() {
+            window.location.href = "https://wa.me/+6281238843858"
+        }
+
+        function redirFacebook() {
+            window.location.href = "https://www.facebook.com/aba.foodz/"
+        }
+
+        function redirGmail() {
+            window.location.href = "https://gmail.com"
+        }
+
+        function redirLinked() {
+            window.location.href = "https://www.linkedin.com/in/mahfud-bamualim-b26a8a254"
+        }
